9c8d78d61139355ec377b5db6487d787b8c32fd5
Hyhyhy1 <nikita-kuleshov-2003@mail.ru>
2024-11-20 19:24:11 +0500

    Reward function added

--- utils/env.py
+++ utils/env.py
@@ -8,13 +8,25 @@ import gymnasium as gym
 
 from gymnasium import spaces
 from collections import defaultdict
+from utils.action_type import Action_type, Action
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
+def get_action_type(action):
+    if action == Action.SERVER_BLOCK_CURRENT_ADDRESS_GROUP:
+        return Action_type.MULTILPE_TARGET_ACTION
+    else:
+        return Action_type.SINGLE_TARGET_ACTION
+
+
 class TrafficEnv(gym.Env):
-    def __init__(self, proxy_host='127.0.0.1', proxy_port=8090, server_host='127.0.0.1', server_port=8080, mode='train'):
+    def __init__(self, proxy_host='127.0.0.1', proxy_port=8090, server_host='127.0.0.1', server_port=8080, mode='train', load_threshold=0.75, hazard_index=1):
         super(TrafficEnv).__init__()
         self.mode = mode
+
+        self.load_threshold = load_threshold
+        self.hazard_index = hazard_index
+        self.request_buffer =[]
 
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(5,), dtype=np.float32)
         self.action_space = spaces.Discrete(6)  # предварительно
@@ -72,6 +84,94 @@ class TrafficEnv(gym.Env):
         info = {}
 
         return None, reward, done, info
+
+
+    def get_reward(self, action):
+        cpu_usage, memory_usage, connection_usage = get_server_usage()
+        max_cpu_usage, max_memory_usage, max_connection_usage = get_max_server_usage()
+        cpu_percent_usage, memory_percent_usage, connection_percent_usage = cpu_usage/max_cpu_usage, memory_usage/max_memory_usage, connection_usage/max_connection_usage
+        max_load = max(cpu_percent_usage, memory_percent_usage, connection_percent_usage)
+        is_heavy_loaded = max_load >= self.load_threshold
+        
+        action_type = get_action_type(action)
+        reward = 0
+        
+        if is_heavy_loaded:
+            if action_type == Action_type.SINGLE_REQUEST_ACTION:
+                if self.request_buffer[0][1] == True and action == Action.SERVER_RECIEVE_CURRENT: #True negative
+                    return reward
+                
+                elif self.request_buffer[0][1] == True and (action == Action.SERVER_DROP_CURRENT or action == Action.SERVER_BLOCK_CURRENT_ADDRESS): #False Positive
+                    reward = -2/(max_load / self.load_threshold * self.hazard_index)
+                    return reward
+                
+                elif self.request_buffer[0][1] == False and action == Action.SERVER_RECIEVE_CURRENT: #False Negative
+                    reward = -(max_load / self.load_threshold * self.hazard_index)
+                    return reward
+                
+                elif self.request_buffer[0][1] == False and (action == Action.SERVER_DROP_CURRENT or action == Action.SERVER_BLOCK_CURRENT_ADDRESS): #True Positive
+                    reward = max_load / self.load_threshold * self.hazard_index
+                    return reward
+                
+                else:
+                    return ValueError(f"Uncnown action: {action}")
+                
+            elif action_type == Action_type.MULTILPE_TARGET_ACTION:
+                if action == Action.SERVER_BLOCK_CURRENT_ADDRESS_GROUP:
+                    fp, tp = 0, 0
+                    target_adress_group = self.request_buffer[0][0].address_group
+                    for i in range(1, len(self.request_buffer)):
+                        if self.request_buffer[i][0].address_group == target_adress_group:
+                            if self.request_buffer[i][1] == True:
+                                fp+=1
+
+                            else:
+                                tp+=1
+                    
+                    reward = (max_load / self.load_threshold * self.hazard_index) * tp - (2 / (max_load / self.load_threshold * self.hazard_index) * fp)
+                    return reward
+
+                else:
+                    return ValueError(f"Uncnown action: {action}")
+        
+        else:
+            if action_type == Action_type.SINGLE_REQUEST_ACTION:
+                if self.request_buffer[0][1] == True and action == Action.SERVER_RECIEVE_CURRENT: #True negative
+                    return reward
+                
+                elif self.request_buffer[0][1] == True and (action == Action.SERVER_DROP_CURRENT or action == Action.SERVER_BLOCK_CURRENT_ADDRESS): #False Positive
+                    reward = -2
+                    return reward
+                
+                elif self.request_buffer[0][1] == False and action == Action.SERVER_RECIEVE_CURRENT: #False Negative
+                    reward = -1
+                    return reward
+                
+                elif self.request_buffer[0][1] == False and (action == Action.SERVER_DROP_CURRENT or action == Action.SERVER_BLOCK_CURRENT_ADDRESS): #True Positive
+                    reward = 1
+                    return reward
+                
+                else:
+                    return ValueError(f"Uncnown action: {action}")
+                
+            elif action_type == Action_type.MULTILPE_TARGET_ACTION:
+                if action == Action.SERVER_BLOCK_CURRENT_ADDRESS_GROUP:
+                    fp, tp = 0, 0
+                    target_adress_group = self.request_buffer[0][0].address_group
+                    for i in range(1, len(self.request_buffer)):
+                        if self.request_buffer[i][0].address_group == target_adress_group:
+                            if self.request_buffer[i][1] == True:
+                                fp+=1
+
+                            else:
+                                tp+=1
+                    
+                    reward = tp - 2 * fp
+                    return reward
+
+                else:
+                    return ValueError(f"Uncnown action: {action}")
+
     
 e = TrafficEnv()
 
